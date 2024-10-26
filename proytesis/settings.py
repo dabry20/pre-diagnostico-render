f3ac33809465be5ad00f0d3b2399fafcf4ae5b64
dabry20 <dabry.521@gmail.com>
2024-10-26 12:09:52 -0500

    setting

--- proytesis/settings.py
+++ proytesis/settings.py
@@ -15,21 +15,18 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import os
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY' , default='asdfddfdsgdsagdsafds')
+SECRET_KEY = 'django-insecure-jh1eb1+1zg&z--hil3ybrisp&c8dc92sgt#h&bjf3wj7fss5*b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
-RENDER_EXTERNA_HOSTNAME= os.environ.get('RENDER_EXTERNA_HOSTNAME')
-if RENDER_EXTERNA_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNA_HOSTNAME)
 
 # Application definition
 
@@ -45,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,9 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-if not DEBUG:
-    STATIC_URL = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
